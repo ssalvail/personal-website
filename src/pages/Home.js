f3db70b60771ee2edeb56e039d022a19c46d4644
Sam Salvail <ssalvail39@gmail.com>
2018-10-22 15:43:45 -0700

    added companies section and projects section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import posed from 'react-pose';
 
 import ProfilePic from '../images/ProfilePic.jpg';
+import GrettaLogo from '../images/gretta_logo.svg';
+import ZengotLogo from '../images/zengot_logo.png';
 
 import './Home.css';
 
@@ -31,6 +33,16 @@ const EmailButton = posed.div({
     hovered: { scale: 1.2 }
 });
 
+const GrettaButton = posed.div({
+    idle: { scale: 1 },
+    hovered: { scale: 1.2 }
+});
+
+const ZengotButton = posed.div({
+    idle: { scale: 1 },
+    hovered: { scale: 1.2 }
+});
+
 class Home extends Component {
 
     constructor(props) {
@@ -41,7 +53,9 @@ class Home extends Component {
             hoveringLinkedin: false,
             hoveringGithub: false,
             hoveringFacebook: false,
-            hoveringEmail: false
+            hoveringEmail: false,
+            hoveringGretta: false,
+            hoveringZengot: false
         };
     }
 
@@ -68,30 +82,30 @@ class Home extends Component {
                     <h2>Front End Engineer</h2>
                 </div>
                 <div className="social-buttons">
-                    <Link to="https://www.linkedin.com/in/ssalvail/" target="_blank" className="linkedin-button">
+                    <a href="https://www.linkedin.com/in/ssalvail/" target="_blank" className="linkedin-button">
                     <LinkedinButton
                         pose={this.state.hoveringLinkedin ? "hovered" : "idle"}
                         onMouseEnter={() => this.setState({ hoveringLinkedin: true })}
                         onMouseLeave={() => this.setState({ hoveringLinkedin: false })}>
                         <i className="fab fa-linkedin-in"></i>
                     </LinkedinButton>
-                    </Link>
-                    <Link to="https://github.com/TheOGPoobah/" target="_blank" className="github-button">
+                    </a>
+                    <a href="https://github.com/ssalvail/" target="_blank" className="github-button">
                     <GithubButton
                         pose={this.state.hoveringGithub ? "hovered" : "idle"}
                         onMouseEnter={() => this.setState({ hoveringGithub: true })}
                         onMouseLeave={() => this.setState({ hoveringGithub: false })}>
                         <i className="fab fa-github"></i>
                     </GithubButton>
-                    </Link>
-                    <Link to="https://www.facebook.com/SamSalvail/" target="_blank" className="facebook-button">
+                    </a>
+                    <a href="https://www.facebook.com/SamSalvail/" target="_blank" className="facebook-button">
                     <FacebookButton
                         pose={this.state.hoveringFacebook ? "hovered" : "idle"}
                         onMouseEnter={() => this.setState({ hoveringFacebook: true })}
                         onMouseLeave={() => this.setState({ hoveringFacebook: false })}>
                         <i className="fab fa-facebook"></i>
                     </FacebookButton>
-                    </Link>
+                    </a>
                     <a href="mailto:ssalvail39@gmail.com" target="_blank" className="email-button">
                     <EmailButton
                         pose={this.state.hoveringEmail ? "hovered" : "idle"}
@@ -100,6 +114,56 @@ class Home extends Component {
                         <i className="far fa-envelope"></i>
                     </EmailButton>
                     </a>
+                </div>
+                <hr className="hr" />
+                <div>
+                    <h3 className="header-center">Companies I Have Worked For</h3>
+                    <div className="companies">
+                        <a href="https://gretta.com/" target="_blank" className="no-decoration">
+                            <GrettaButton className="gretta"
+                                pose={this.state.hoveringGretta ? "hovered" : "idle"}
+                                onMouseEnter={() => this.setState({ hoveringGretta: true })}
+                                onMouseLeave={() => this.setState({ hoveringGretta: false })}>
+                                <img className="gretta-img" src={GrettaLogo} alt="gretta logo"/>
+                                <div>gretta</div>
+                            </GrettaButton>
+                        </a>
+                        <a href="https://www.zengot.com/" target="_blank" className="no-decoration">
+                            <ZengotButton className="zengot"
+                                pose={this.state.hoveringZengot ? "hovered" : "idle"}
+                                onMouseEnter={() => this.setState({ hoveringZengot: true })}
+                                onMouseLeave={() => this.setState({ hoveringZengot: false })}>
+                                <img className="zengot-img" src={ZengotLogo} alt="zenGOT logo"/>
+                            </ZengotButton>
+                        </a>
+                    </div>
+                </div>
+                <hr className="hr" />
+                <div>
+                    <h3 className="header-center">Projects and Events</h3>
+                    <div className="projects">
+                        <div className="project-items">
+                            <h4>HoloJam - March 2016</h4>
+                            <ul>
+                                <li>Created a proof of concept demo for the <a href="https://hplustech.com/" target="_blank">Holus</a></li>
+                                <li>Tied for first place prize</li>
+                                <li><a href="https://vimeo.com/170048050" target="_blank">HoloJam video</a></li>
+                            </ul>
+                        </div>
+                        <div className="project-items">
+                            <h4>NWHacks - February 2016</h4>
+                            <ul>
+                                <li>Created a <a href="http://devpost.com/software/liveinlife-jz1rfv" target="_blank">web and mobile app</a> with a team of five in 24 hours</li>
+                                <li>Placed in the top six teams out of over 90 submissions</li>
+                            </ul>
+                        </div>
+                        <div className="project-items">
+                            <h4>Global Game Jam 2016 - January 2016</h4>
+                            <ul>
+                                <li>Created a <a href="http://globalgamejam.org/2016/games/blood-moon-0" target="_blank">2D platformer</a> with a team of three in 48 hours</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <hr className="hr" />
                 <div className="footer">
